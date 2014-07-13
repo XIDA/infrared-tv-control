b1d21bcaf2af6f1c52f59ab6fc616a97e3213e17
BenjaminKobjolke <b.kobjolke@xida.de>
2014-07-13 15:44:35 +0200

    fixed problems in windows tool setup

--- windows/ircontrol/resources/en.Designer.cs
+++ windows/ircontrol/resources/en.Designer.cs
@@ -39,7 +39,7 @@ namespace ircontrol {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("dmxcontrol.en", typeof(en).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ircontrol.en", typeof(en).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
